896b6be21f612739694b93b34cb993cc02c07809
xacaag <xacaa20@gmail.com>
2021-07-23 17:51:29 +0800

    firebase

---
+++ app.js
@@ -0,0 +1,15 @@
+function shuffle(array) {
+    var currentIndex = array.length,  randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
