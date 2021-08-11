2ce80a2024ff0a8249b4b24ccb93bd16e05722db
Tuguldur <tuguldur@bayaraaEs-iMac.local>
2021-08-11 17:32:01 +0800

    leaderboard

---
+++ public/js/leaderboard.js
@@ -0,0 +1,25 @@
+db.collection("score")
+  .orderBy("score", "desc")
+  .limit(8)
+  .get()
+  .then((querySnapshot) => {
+    let i = 1;
+    querySnapshot.forEach((doc) => {
+      let container = document.createElement("div");
+      container.classList.add("row");
+      let heddeh = document.createElement("div");
+      let name = document.createElement("div");
+      let score = document.createElement("div");
+      heddeh.classList.add("TenOfTen");
+      heddeh.innerHTML = "#" + i;
+      name.classList.add("name");
+      name.innerHTML = doc.data().name;
+      score.classList.add("score");
+      score.innerHTML = doc.data().score;
+      container.appendChild(heddeh);
+      container.appendChild(name);
+      container.appendChild(score);
+      document.getElementsByClassName("cololumn")[0].appendChild(container);
+      i++;
+    });
+  });
