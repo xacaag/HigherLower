f80cff5e34f5d5b28db61f4089a38b5f64f36cad
orgil7860 <duskyorgil@gmail.com>
2021-08-11 17:03:53 +0800

    Countdown

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 
 let data = [];
+let timeLeft;
 
 db.collection("movie").get().then((docs) => {
   docs.forEach((doc) => {
@@ -24,6 +25,7 @@ let R_BUTTON = document.getElementById("higher")
 let L_BUTTON = document.getElementById("lower")
 let link1 = document.getElementById("link")
 let link2 = document.getElementById("link2")
+
 let i = 0;
 
 
@@ -45,8 +47,9 @@ setTimeout(() => {
   link2.href = data[i+1].photo;  
   link1.innerHTML = data[i].name;
   link2.innerHTML = data[i+1].name;  
-  console.log(link1)
-  console.log(link2)
+ 
+  VS2.classList.add("invisible");
+  intervala();
 }, 1000);
 
  
@@ -54,22 +57,6 @@ setTimeout(() => {
 const start = () => {
    location.href="./classic.html "
   }
-
-  // setTimeout(() => {
-  //   followers1.innerHTML = data[i].imdb.toLocaleString();
-  //   followers2.innerHTML = data[i+1].imdb.toLocaleString();
-  //   name1.innerHTML = data[i].name;
-  //   name2.innerHTML = data[i+1].name;
-  //   container1.style.backgroundImage = `url(${data[i].photo})`
-  //   container2.style.backgroundImage = `url(${data[i+1].photo})`
-  //   container1.style.backgroundSize = "cover";
-  //   container2.style.backgroundSize = "cover";
-  //   container1.style.transitionDelay ="2.5s";
-  //   container2.style.transitionDelay ="2.5s";
-  //   link1.href = data[i].photo;
-  //   link2.href = data[i+1].photo;    
-  // }, 1000);
-
 
 
 
@@ -89,12 +76,14 @@ const higher = () => {
     link1.innerHTML = data[i].name;
     link2.innerHTML = data[i+1].name;  
     i++;
-
+    reset()
   }else{
     followers2.innerHTML = data[i+1].imdb.toLocaleString();
     setTimeout(function(){ wrong_animation_1();}, 300)
     setTimeout(function(){ location.href="./gameover.html";;}, 2500)
   }
+  
+ 
 }
 
   const lower = () => {
@@ -107,16 +96,19 @@ const higher = () => {
       followers2.innerHTML = data[i+1].imdb.toLocaleString();
       link1.href = data[i+1].photo;
       link2.href = data[i+2].photo;
-      
-      correct_animation_1();
+      correct_animation_1(); 
       i++;
-      
+      reset();
     }else{
     followers2.innerHTML = data[i+1].imdb.toLocaleString();
     setTimeout(function(){ wrong_animation_1();}, 300)
     setTimeout(function(){ location.href="./gameover.html";;}, 2500)
     }
   }
+  
+
+
+  
   var Answer;
     var isplayercorrect;
     let high_score = document.getElementById("hi_score")
@@ -125,24 +117,28 @@ const higher = () => {
     // in answer true = higher , false = lower
     // activating animations
     let VS = document.getElementById("vs_id");
+    let VS2 = document.getElementById("vs_id2");
     let BUTTON = document.getElementById("button_id")
     
 
     function correct_animation_1() {
-       R_BUTTON.disabled = true;
+        R_BUTTON.disabled = true;
         L_BUTTON.disabled = true;
+        VS.innerHTML = "";
         setTimeout(function(){   R_BUTTON.disabled = true;;},0)
         setTimeout(function(){   L_BUTTON.disabled = true;;},0)
-        setTimeout(function(){ VS.innerHTML = ("");;}, 1000)
-        setTimeout(function(){ VS.classList.add('vs_correct');},0)
+        setTimeout(function(){  VS2.classList.add("vs_correct");},0)
+        setTimeout(function(){ VS2.classList.remove('invisible');},0)
+        setTimeout(function(){ VS2.classList.add('invisible');},4000)
         setTimeout(function(){ BUTTON.classList.add('button_dissapear');},0)
         setTimeout(animation_3, 300);
         setTimeout(animation_4, 2000);
-        setTimeout(correct_animation_1_2, 3000);
         setTimeout(correct_animation_2, 4000);
         setTimeout(function(){  R_BUTTON.disabled = false;},4000)
         setTimeout(function(){   L_BUTTON.disabled = false;},4000)
+        VS.classList.remove("invisible")
     }
+
     function correct_animation_1_2() {
         VS.innerHTML = ("VS")
     }
@@ -153,18 +149,15 @@ const higher = () => {
         VS.classList.remove('vs_correct');
     }
     function animation_3() {
+      
         followers2.classList.remove("invisible");
         R_TEXT.classList.remove("invisible")
         followers2.classList.add("right_ammount_appear");
         R_TEXT.classList.add("right_ammount_appear")
-        // setTimeout(function(){ followers2.classList.remove("invisible");}, 3000)
-        // setTimeout(function(){ R_TEXT.classList.remove("invisible");},3000)
+    
     }
     function animation_4() {
-        // followers2.classList.add("invisible");
-        // R_TEXT.classList.add("invisible");
-      //  followers2.classList.remove("right_ammount_appear");
-      //   R_TEXT.classList.remove("right_ammount_appear")
+     
         setTimeout(function(){ followers2.classList.add("invisible");}, 500)
         setTimeout(function(){ R_TEXT.classList.add("invisible");},500)
         setTimeout(function(){ followers2.classList.remove("right_ammount_appear");;}, 2200)
@@ -172,15 +165,32 @@ const higher = () => {
         
     }
     function wrong_animation_1() {
-      setTimeout(function(){ VS.innerHTML = ("");;}, 0)
-      setTimeout(function(){ VS.classList.add('vs_wrong');},0)
+      // setTimeout(function(){ VS.innerHTML = ("");;}, 0)
+      // setTimeout(function(){ VS.classList.add('vs_wrong');},0)
+      setTimeout(function(){  VS2.classList.add("vs_wrong");},1000)
+        setTimeout(function(){ VS2.classList.remove('invisible');},0)
+        setTimeout(function(){ VS2.classList.add('invisible');},4000)
       setTimeout(function(){ BUTTON.classList.add('button_dissapear');;},0)
       setTimeout(animation_3, 300);
-      // setTimeout(wrong_animation_2, 2500);
+
     }
-    // const L_BUTTON = document.getElementById("lower")
-    // const R_BUTTON = document.getElementById("higher") 
-    // function button_enable() {
-    //     R_BUTTON.disabled = false;
-    //     L_BUTTON.disabled = false;
-    // }
+  
+    timeLeft = 10
+  
+  let intervala = setInterval(() => {
+    if(timeLeft === 0 ){
+      followers2.innerHTML = data[i+1].imdb.toLocaleString();
+        setTimeout(function(){ wrong_animation_1();}, 300)  
+        setTimeout(function(){ location.href="./gameover.html";;}, 2500)
+    }else{
+      VS.innerHTML = String( timeLeft );
+      timeLeft--;
+    }
+  }, 1000)
+ 
+  
+
+let reset = () => {
+  timeLeft = 12;
+}
+
