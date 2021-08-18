e69a6ab5c6dcf6844d4c28b4862167065baad975
javkhaa <javkhaani@gmail.com>
2021-08-18 10:21:41 +0800

    bugfix

--- public/js/room.js
+++ public/js/room.js
@@ -1,64 +1,44 @@
 const randomIdGenerator = () => {
-    var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
-    var uniqid = randLetter + Date.now();
-    return uniqid;
-  };
-  
-  const createroom = async () => {
-    document.getElementById('loader').style.visibility = 'visible';
-    // let roomName = document.getElementById("room-input").innerText;
-    let ref = await db.collection("rooms").add({
-      // roomName: roomName,
-      players: 0,
-      ready: false,
-      start: false,
-      time: firebase.firestore.FieldValue.serverTimestamp(),
-    });  
-    await db
-      .collection("rooms")
-      .get()
-      .then((doc) => {
-        doc.forEach((el) => {
-          let playersNumber = el.data().players;
-          playersNumber++;
-          db.collection("rooms").doc(el.id).update({
-            players: playersNumber,
-          });
-        });
-      });
-  
-    let uid = randomIdGenerator().slice(8);
-    await db.doc(`links/${uid}`).set({
-      link: ref.id,
-    });
-    window.location = `lobby.html?roomid=${uid}`;
-  };
-  
-  const join = async () => {
-    let realcode;
-    const joincode = document.getElementById("join").value;
-  
-    await db
-      .collection("links")
-      .doc(`${joincode}`)
-      .get()
-      .then((el) => {
-        realcode = el.data().link;
-      });
-  
-    await db
-      .collection("rooms")
-      .doc(`${realcode}`)
-      .get()
-      .then((el) => {
-        let playersNumber = el.data().players;
-        playersNumber++;
-        db.collection("rooms").doc(el.id).update({
-          players: playersNumber,
-          ready: true
-        });
-        setTimeout(() => {
-          window.location = `lobby.html?roomid=${joincode}`;
-        }, 500);
-      });
-  };
+  var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  var uniqid = randLetter + Date.now();
+  return uniqid;
+};
+
+const createroom = async () => {
+  document.getElementById("loader").style.visibility = "visible";
+  let ref = await db.collection("rooms").add({
+    players: 1,
+    ready: false,
+    start: false,
+    time: firebase.firestore.FieldValue.serverTimestamp(),
+  });
+
+  let uid = randomIdGenerator().slice(8);
+  await db.doc(`links/${uid}`).set({
+    link: ref.id,
+  });
+  window.location = `lobby.html?roomid=${uid}`;
+};
+
+const join = async () => {
+  const joincode = document.getElementById("join").value;
+
+  let doc = await db.collection("links").doc(`${joincode}`).get();
+
+  let { link } = doc.data();
+
+  let roomData = await db.collection("rooms").doc(`${link}`).get();
+
+  let { players } = roomData.data();
+  await db
+    .collection("rooms")
+    .doc(`${link}`)
+    .set(
+      {
+        players: ++players,
+        ready: true,
+      },
+      { merge: true }
+    );
+  window.location = `lobby.html?roomid=${joincode}`;
+};
