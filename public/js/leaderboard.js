1caef9338b009e2f4f6267b770191eaa8c726a65
javkhaa <javkhaani@gmail.com>
2021-09-02 17:19:02 +0800

    def

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -4,28 +4,35 @@ db.collection("users")
   .onSnapshot((querySnapshot) => {
     let i = 1;
     querySnapshot.forEach((doc) => {
-      let container = document.createElement("div");
-      container.classList.add("row");
-      let heddeh = document.createElement("div");
-      let name = document.createElement("div");
-      let score = document.createElement("div");
-      heddeh.classList.add("TenOfTen");
-      heddeh.innerHTML = "#" + i;
-      name.classList.add("name");
-      name.innerHTML = doc.data().name;
-      score.classList.add("score");
-      score.innerHTML = doc.data().score;
-      container.appendChild(heddeh);
-      container.appendChild(name);
-      container.appendChild(score);
-      document.getElementsByClassName("cololumn")[0].appendChild(container);
-      document.getElementById("loader").style.display = "none";
-      i++;
+      if (
+        doc.data().name.includes("<a href") &&
+        doc.data().score.includes("<a href")
+      ) {
+        console.log("haha");
+      } else {
+        let container = document.createElement("div");
+        container.classList.add("row");
+        let heddeh = document.createElement("div");
+        let name = document.createElement("div");
+        let score = document.createElement("div");
+        heddeh.classList.add("TenOfTen");
+        heddeh.innerHTML = "#" + i;
+        name.classList.add("name");
+        name.innerHTML = doc.data().name;
+        score.classList.add("score");
+        score.innerHTML = doc.data().score;
+        container.appendChild(heddeh);
+        container.appendChild(name);
+        container.appendChild(score);
+        document.getElementsByClassName("cololumn")[0].appendChild(container);
+        document.getElementById("loader").style.display = "none";
+        i++;
+      }
     });
   });
 const back = () => {
   location.href = "./index.html ";
 };
-  const back_index = () => {
-    window.location = "index.html";
-  }
+const back_index = () => {
+  window.location = "index.html";
+};
